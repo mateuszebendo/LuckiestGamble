e3562b1ec7d42fff3741fdd5bee47fab81b499f2
mateuszebendo <mateuszebendovp30@gmail.com>
2025-06-03 22:40:39 -0300

    feat: adiciona bootstrap, jquery e componentizacao basica

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,9 @@
+$("#btn-login").click(function () {
+    $("#form-cadastro").show();
+    $("#form-login").hide();
+});
+
+$("#btn-cadastro").click(function () {
+    $("#form-cadastro").hide();
+    $("#form-login").show();
+});
